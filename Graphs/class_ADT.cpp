e58fa258e5e684482d054ed52e63f4e43e517e75
codernotfound-219 <aswatt219@icloud.com>
2024-11-05 01:03:20 +0530

    Update class_adt.cpp

--- Graphs/class_ADT.cpp
+++ Graphs/class_ADT.cpp
@@ -1,138 +1,215 @@
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
 #include <list>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
 class Graph {
 private:
-  vector<int> vertex;
-  vector<vector<int>> adj_mat;
-  vector<list<int>> adj_list;
-  vector<pair<int, int>> edge;
-  int index;
+  vector<list<int> > adjList;
+  vector<vector<int> > adjMatrix;
+  vector<int> vertices;
+  vector<pair<int, int> > edges;
 
 public:
-  Graph(int n) {
-    for (int i = 0; i < n; i++)
-      vertex.push_back(0);
-    for (int i = 0; i < n; i++) {
-      vector<int> temp;
-      for (int i = 0; i < n; i++)
-        temp.push_back(0);
-      adj_mat.push_back(temp);
+  Graph() {}
+
+  void addVertex(int vertex) {
+    if (find(vertices.begin(), vertices.end(), vertex) != vertices.end()) {
+      cout << "Vertex " << vertex << " already exists." << endl;
+      return;
     }
-    for (int i = 0; i < n; i++) {
-      list<int> temp;
-      adj_list.push_back(temp);
+
+    vertices.push_back(vertex);
+    adjList.push_back(list<int>());
+
+    // Resize adjacency matrix
+    for (size_t i = 0; i < adjMatrix.size(); ++i) {
+      adjMatrix[i].push_back(0);
     }
-    for (int i = 0; i < n; i++) {
-      pair<int, int> p(-1, -1);
-      edge.push_back(p);
+    adjMatrix.push_back(vector<int>(vertices.size(), 0));
+  }
+
+  void addEdge(int from, int to) {
+    vector<int>::iterator fromIt =
+        find(vertices.begin(), vertices.end(), from);
+    vector<int>::iterator toIt =
+        find(vertices.begin(), vertices.end(), to);
+
+    if (fromIt == vertices.end() || toIt == vertices.end()) {
+      cout << "One or both vertices do not exist." << endl;
+      return;
     }
-    index = -1;
+
+    int fromIndex = distance(vertices.begin(), fromIt);
+    int toIndex = distance(vertices.begin(), toIt);
+
+    adjList[fromIndex].push_back(to);
+    adjMatrix[fromIndex][toIndex] = 1;
+
+    edges.push_back(make_pair(from, to));
   }
 
-  bool add_vertex(int v) {
-    vertex[++index] = v;
-    return true;
-  }
-
-private:
-  bool add_edge(int v, int i1, int i2) {
-    if (i1 >= 0 && i1 < vertex.size() && i2 >= 0 && i2 < vertex.size()) {
-      edge[v].first = i1;
-      edge[v].second = i2;
-      return true;
+  void deleteVertex(int vertex) {
+    vector<int>::iterator vIt =
+        find(vertices.begin(), vertices.end(), vertex);
+    if (vIt == vertices.end()) {
+      cout << "Vertex " << vertex << " does not exist." << endl;
+      return;
     }
-    return false;
-  }
 
-  bool update_matrix(int i1, int i2) {
-    adj_mat[i1][i2] = 1;
-    adj_mat[i2][i1] = 0;
-    return true;
-  }
+    int index = distance(vertices.begin(), vIt);
 
-  bool update_list(int i1, int i2) {
-    adj_list[i1].push_back(i2);
-    adj_list[i2].push_back(i1);
-    return true;
-  }
-
-public:
-  bool add_edge(int i1, int i2) {
-    if (!add_edge(i1 - 1, i2 - 1))
-      return false;
-    if (!update_matrix(i1 - 1, i2 - 1))
-      return false;
-    if (!update_list(i1 - 1, i2 - 1))
-      return false;
-    return true;
-  }
-
-  void printList() {
-    for (int i = 1; i <= adj_list.size(); i++) {
-      cout << i << "\t -> ";
-      list<int>::iterator it;
-
-      for (it = adj_list[i].begin(); it != adj_list[i].end(); ++it) {
-        cout << *it << " -> ";
+    // Remove edges connected to this vertex
+    vector<pair<int, int> >::iterator eIt = edges.begin();
+    while (eIt != edges.end()) {
+      if (eIt->first == vertex || eIt->second == vertex) {
+        eIt = edges.erase(eIt);
+      } else {
+        ++eIt;
       }
+    }
 
+    // Remove vertex from vertices list
+    vertices.erase(vIt);
+
+    // Remove from adjacency list
+    adjList.erase(adjList.begin() + index);
+    for (size_t i = 0; i < adjList.size(); ++i) {
+      adjList[i].remove(vertex);
+    }
+
+    // Remove from adjacency matrix
+    adjMatrix.erase(adjMatrix.begin() + index);
+    for (size_t i = 0; i < adjMatrix.size(); ++i) {
+      adjMatrix[i].erase(adjMatrix[i].begin() + index);
+    }
+  }
+
+  void deleteEdge(int from, int to) {
+    vector<int>::iterator fromIt =
+        find(vertices.begin(), vertices.end(), from);
+    vector<int>::iterator toIt =
+        find(vertices.begin(), vertices.end(), to);
+
+    if (fromIt == vertices.end() || toIt == vertices.end()) {
+      cout << "One or both vertices do not exist." << endl;
+      return;
+    }
+
+    int fromIndex = distance(vertices.begin(), fromIt);
+    int toIndex = distance(vertices.begin(), toIt);
+
+    adjList[fromIndex].remove(to);
+    adjMatrix[fromIndex][toIndex] = 0;
+
+    vector<pair<int, int> >::iterator eIt =
+        find(edges.begin(), edges.end(), make_pair(from, to));
+    if (eIt != edges.end()) {
+      edges.erase(eIt);
+    }
+  }
+
+  void printAdjacencyMatrix() {
+    cout << "Adjacency Matrix:" << endl;
+    cout << "     ";
+    for (size_t i = 0; i < vertices.size(); i++) {
+      cout << setw(2) << vertices[i] << " ";
+    }
+    cout << endl;
+
+    for (size_t i = 0; i < vertices.size(); i++) {
+      cout << setw(2) << vertices[i] << " | ";
+      for (size_t j = 0; j < vertices.size(); j++) {
+        cout << setw(2);
+        if (adjMatrix[i][j] == 0) {
+          cout << "-" << " ";
+        } else {
+          cout << adjMatrix[i][j] << " ";
+        }
+      }
       cout << endl;
     }
   }
 
-  void printMatrix() {
-    cout << "\t";
-    for (int i = 0; i < adj_mat.size(); i++)
-      cout << i + 1 << " ";
-    cout << endl;
-    for (int i = 0; i < adj_mat.size(); i++) {
-      cout << i + 1 << "\t";
-      for (int j = 0; j < adj_mat[i].size(); j++) {
-        cout << adj_mat[i][j] << ' ';
+  void printAdjacencyList() {
+    cout << "Adjacency List:" << endl;
+    for (size_t i = 0; i < vertices.size(); i++) {
+      cout << vertices[i] << " -> ";
+      for (list<int>::const_iterator it = adjList[i].begin();
+           it != adjList[i].end(); ++it) {
+        cout << *it << " ";
       }
-
       cout << endl;
     }
   }
 
-  void printVertex() {
-    for (int i = 0; i < vertex.size(); i++) {
-      cout << vertex[i] << " ";
+  void printVertices() {
+    cout << "Vertices: ";
+    for (vector<int>::const_iterator it = vertices.begin();
+         it != vertices.end(); ++it) {
+      cout << *it << " ";
     }
     cout << endl;
   }
 
-  void printEdge() {
-    for (int i = 0; i < edge.size(); i++) {
-      cout << "(" << edge[i].first << ", " << edge[i].second << ")" << endl;
+  void printEdges() {
+    cout << "Edges: ";
+    for (vector<pair<int, int> >::const_iterator it = edges.begin();
+         it != edges.end(); ++it) {
+      cout << "(" << it->first << "," << it->second << ") ";
     }
+    cout << endl;
   }
 };
 
 int main() {
-  Graph g(5);
+  Graph g;
 
-  g.add_vertex(1);
-  g.add_vertex(2);
-  g.add_vertex(3);
-  g.add_vertex(4);
-  g.add_vertex(5);
+  // Add vertices
+  g.addVertex(1);
+  g.addVertex(3);
+  g.addVertex(5);
+  g.addVertex(7);
 
-  g.printVertex();
+  // Add edges
+  g.addEdge(1, 3);
+  g.addEdge(3, 5);
+  g.addEdge(5, 7);
+  g.addEdge(7, 1);
 
-  g.add_edge(1, 2);
-  g.add_edge(2, 5);
-  g.add_edge(2, 3);
-  g.add_edge(3, 4);
-  g.add_edge(4, 5);
+  // Print initial graph
+  cout << "Initial Graph:" << endl;
+  g.printVertices();
+  g.printEdges();
+  g.printAdjacencyMatrix();
+  cout << endl;
+  g.printAdjacencyList();
+  cout << endl;
 
-  g.printEdge();
-  cout << "ADJACENCY MATRIX: " << endl;
-  g.printMatrix();
-  cout << "\nADJACENCY LIST:" << endl;
-  g.printList();
+  // Delete an edge
+  g.deleteEdge(3, 5);
+
+  // Add a new vertex
+  g.addVertex(10);
+
+  // Add edges to the new vertex
+  g.addEdge(5, 10);
+  g.addEdge(10, 3);
+
+  // Delete a vertex
+  g.deleteVertex(7);
+
+  // Print updated graph
+  cout << "Updated Graph:" << endl;
+  g.printVertices();
+  g.printEdges();
+  g.printAdjacencyMatrix();
+  cout << endl;
+  g.printAdjacencyList();
+
+  return 0;
 }
